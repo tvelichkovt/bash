2527872981f060c9cde861cd0d4172d2ad0b50b2
Todor <tvelichkovt@gmail.com>
2021-08-16 09:37:32 +0300

    BASH Shell Scripting

---
+++ Automating user management with bash script/regex - if the test returns true, the substring is contained in the string.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# regex - if the test returns true, the substring is contained in the string
+STR='Earth is flat'
+SUB='flat'
+
+if [[ "$STR" =~ .*"$SUB".* ]]; then
+  echo "yep, this is so true!"
+fi
